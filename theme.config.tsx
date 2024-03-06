8ad12234447a6185bb32e82eaafde97bb5bf8ef1
cu8code <git8ankanroy@gmail.com>
2024-03-06 23:43:27 +0530

    change

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-  logo: <span>asansol/ui</span>,
+  logo: <span>Baxor/ui</span>,
   project: {
     link: 'https://github.com/shuding/nextra-docs-template',
   },
