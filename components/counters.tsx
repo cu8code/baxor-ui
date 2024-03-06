d2d059421642b34d007e3c6e326ed1abe579d94f
cu8code <git8ankanroy@gmail.com>
2024-03-07 00:05:26 +0530

    added tailwindcss

--- components/counters.tsx
+++ components/counters.tsx
@@ -13,7 +13,7 @@ function MyButton() {
   return (
     <div>
       <button onClick={handleClick} className={styles.counter}>
-        Clicked {count} times
+        <span className='text-blue-500'>Clicked</span> {count} times
       </button>
     </div>
   )
