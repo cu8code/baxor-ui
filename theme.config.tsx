679dceabdb7f6f3503f39857edae3f074b3f136f
cu8code <git8ankanroy@gmail.com>
2024-03-07 00:10:47 +0530

    changed the logo on thene.config.tsx

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-  logo: <span>Baxor/ui</span>,
+  logo: <span>baxor/ui</span>,
   project: {
     link: 'https://github.com/shuding/nextra-docs-template',
   },
